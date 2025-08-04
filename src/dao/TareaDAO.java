66587815a17866488e2f16bebfa20f910ebc2b69
User <User@CristianRojas.lan>
2025-08-03 20:03:27 -0600

    git commit 12 " Se creo arreglo las clases DAO "

--- src/dao/TareaDAO.java
+++ src/dao/TareaDAO.java
@@ -6,6 +6,7 @@ package dao;
 
 import modelo.Tarea;
 
+import javax.swing.JOptionPane;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,7 +35,28 @@ public class TareaDAO {
         }
     }
 
-
+    public boolean actualizar(Tarea tarea) {
+        String sql = "UPDATE Tareas SET IdProyecto = ?, Titulo = ?, Descripcion = ?, FechaInicio = ?, FechaFin = ?, Estado = ?, PorcentajeAvance = ? WHERE Id = ?";
+        try (Connection conn = ConexionBD.getConexion();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, tarea.getIdProyecto());
+            ps.setString(2, tarea.getTitulo());
+            ps.setString(3, tarea.getDescripcion());
+            ps.setDate(4, new java.sql.Date(tarea.getFechaInicio().getTime()));
+            if (tarea.getFechaFin() != null) {
+                ps.setDate(5, new java.sql.Date(tarea.getFechaFin().getTime()));
+            } else {
+                ps.setNull(5, Types.DATE);
+            }
+            ps.setString(6, tarea.getEstado());
+            ps.setInt(7, tarea.getPorcentajeAvance());
+            ps.setInt(8, tarea.getId());
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
     public boolean eliminar(int id) {
         String sql = "DELETE FROM Tareas WHERE Id = ?";
@@ -95,4 +117,6 @@ public class TareaDAO {
         }
         return lista;
     }
+
+
 }
